54ac23159656544a33770f625d9a3c3a9dc7c616
ChLinh <93479230+ChLinh@users.noreply.github.com>
2023-06-05 16:07:16 +0700

    Add files via upload

---
+++ GiaoDienKhachHang/PTTK/DatPhong.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace PTTK_GIaoDien
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2Button6_Click(object sender, EventArgs e)
+        {
+            Connection.Connect();
+            if (guna2ComboBox1.Text == "")
+            {
+                MessageBox.Show("Vui lòng chọn loại phòng cần tìm");
+            }
+            else
+            {
+                //insert DatPhong ( MaPhong, CMND, NgayDP, SoDemLuuTru,MaDoan, SoLuongNguoi) values ( '103', '0987', 350000,CONVERT(DATE,'27-04-2002' ,105), (SELECT DATEDIFF(day, '2023/07/02', '2023/05/09') AS difference),'2',6);
+
+                string sql = "insert DatPhong ( MaPhong, CMND, NgayDP, SoDemLuuTru,MaDoan, SoLuongNguoi, TienCoc) values ( '" + textBox1.Text + "', '" + textBox7.Text + "', CONVERT(DATE, '" + guna2DateTimePicker1.Text + "', 120), " + "(SELECT DATEDIFF(day, '" + guna2DateTimePicker1.Text + "' , '" + guna2DateTimePicker2.Text + "') AS difference)," + "'" + textBox8.Text + "'," + textBox6.Text + ", (SELECT " + textBox4.Text + "*" + " (SELECT DATEDIFF(day, '" + guna2DateTimePicker1.Text + "' , '" + guna2DateTimePicker2.Text + "') AS difference)" + " * (0.5) AS result) );";
+                MessageBox.Show(sql);
+                Connection.RunSQL(sql);
+                sql = "UPDATE Phong SET TrangThaiDat = 1 WHERE MaPhong = " + textBox1.Text + ";";
+                Connection.RunSQL(sql);
+                dataGridView1.DataSource = Connection.GetDataToTable("select * from Phong where TrangThaiDat = '0'");
+                MessageBox.Show(sql);
+            }
+        }
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            Connection.Connect();
+            if (guna2ComboBox1.Text == "")
+            {
+                MessageBox.Show("Vui lòng chọn loại phòng cần tìm");
+            }
+            else
+            {
+                string sql = " select * from Phong where LoaiPhong = '" + guna2ComboBox1.Text + "' and TrangThaiDat ='0'";
+                string check = Connection.GetFieldValues(sql);
+                if (check == "")
+                {
+                    MessageBox.Show("Loại phòng này đã hết");
+                }
+                else dataGridView1.DataSource = Connection.GetDataToTable(sql);
+            }
+        }
+        private void guna2ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dataGridView1.CurrentRow.Index;
+            textBox1.Text = dataGridView1.Rows[i].Cells[0].Value.ToString(); // mã ID
+            textBox2.Text = dataGridView1.Rows[i].Cells[1].Value.ToString(); // LoaiPhong
+            textBox3.Text = dataGridView1.Rows[i].Cells[2].Value.ToString(); // TrangThaiDat
+            textBox4.Text = dataGridView1.Rows[i].Cells[3].Value.ToString(); // Trạng thái vệ sinh
+            textBox5.Text = dataGridView1.Rows[i].Cells[4].Value.ToString(); // Giá
+            textBox6.Text = dataGridView1.Rows[i].Cells[5].Value.ToString(); // Số lượng người
+        }
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void guna2DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void guna2Button10_Click(object sender, EventArgs e)
+        {
+            if (textBox7.Text == "")
+            {
+                MessageBox.Show("Vui điền vào cmnd");
+            }
+            else
+            {
+                Connection.Connect();
+                string sql = " select * from DatPhong where CMND = '" + textBox7.Text + "'";
+                dataGridView2.DataSource = Connection.GetDataToTable(sql);
+                Connection.Disconnect();
+            }
+
+
+
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+
+            i = dataGridView2.CurrentRow.Index;
+            textBox9.Text = dataGridView2.Rows[i].Cells[0].Value.ToString(); // mã DatPhòng
+        }
+
+        private void guna2Button9_Click(object sender, EventArgs e)
+        {
+            Connection.Connect();
+            if (textBox7.Text == "")
+            {
+                MessageBox.Show("Vui điền vào mã hủy đặt phòng");
+            }
+            else
+            {
+                string sql = "delete DatPhong where MADP = '" + textBox9.Text + "'";
+                Connection.RunSQL(sql);
+                sql = "UPDATE Phong SET TrangThaiDat = 0 WHERE MaPhong = (select MaPhong from DatPhong where MaDP = '" + textBox9.Text + "'); ";
+                MessageBox.Show("Hủy đặt phòng thành công");
+            }
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ GiaoDienKhachHang/PTTK/Menu.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace PTTK
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            groupBox1 = new GroupBox();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(189, 109);
+            button1.Margin = new Padding(2, 1, 2, 1);
+            button1.Name = "button1";
+            button1.Size = new Size(169, 22);
+            button1.TabIndex = 0;
+            button1.Text = "Check In";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(189, 133);
+            button2.Margin = new Padding(2, 1, 2, 1);
+            button2.Name = "button2";
+            button2.Size = new Size(169, 22);
+            button2.TabIndex = 1;
+            button2.Text = "Dịch vụ khách sạn";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(560, 322);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // button5
+            // 
+            button5.Location = new Point(189, 85);
+            button5.Margin = new Padding(2, 1, 2, 1);
+            button5.Name = "button5";
+            button5.Size = new Size(169, 22);
+            button5.TabIndex = 4;
+            button5.Text = "Đặt phòng";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(189, 61);
+            button4.Margin = new Padding(2, 1, 2, 1);
+            button4.Name = "button4";
+            button4.Size = new Size(169, 22);
+            button4.TabIndex = 3;
+            button4.Text = "Nhập thông tin đoàn";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(189, 37);
+            button3.Margin = new Padding(2, 1, 2, 1);
+            button3.Name = "button3";
+            button3.Size = new Size(169, 22);
+            button3.TabIndex = 2;
+            button3.Text = "Nhập thông tin khách hàng";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 346);
+            Controls.Add(groupBox1);
+            Margin = new Padding(2, 1, 2, 1);
+            Name = "Menu";
+            Text = "Menu";
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private GroupBox groupBox1;
+        private Button button3;
+        private Button button5;
+        private Button button4;
+    }
+}
